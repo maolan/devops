8140db4b5251c6ef037f06aafd60521c5773c8b5
Rajko Dervisevic <rajkod@hotmail.com>
2019-07-02 16:45:37 +0200

    minor error fix

--- shell/provision.sh
+++ shell/provision.sh
@@ -6,7 +6,7 @@ pkg install -y ccache xorg font-adobe-100dpi git-lite autoconf libsndfile
 cp ${MY_PATH}/.cshrc ~devel/.cshrc
 chown devel:devel ~devel/.cshrc
 
-if [ ! -d /usr/src/fisean ]; then
+if [ ! -d /usr/src/libmaolan ]; then
   git clone https://github.com/maolan/libmaolan /usr/src/libmaolan
   chown -R devel:devel /usr/src/libmaolan
 fi

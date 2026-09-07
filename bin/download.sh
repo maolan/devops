600dcfd3e05d9d5d1f9f7b39c33491f9bbfd63d2
Goran Mekić <meka@sys.it.com>
2026-09-07 12:37:23 +0200

    Add clap

--- bin/download.sh
+++ bin/download.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BIN_DIR=$(dirname ${0})
-REPOS="baseview doc editor engine generate llama-burn lv2 maolan mixosc plugin-protocol plugins trainer vocal widgets"
+REPOS="baseview clap doc editor engine generate llama lv2 maolan mixosc plugin-protocol plugins trainer vocal widgets"
 
 cd "${BIN_DIR}/.."
 for repo in ${REPOS}; do
